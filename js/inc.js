02c68855d5ee02946e43f15ed8f4738771873d09
yonghoon1013 <jyh10105@gmail.com>
2023-08-08 14:52:04 +0900

    1

---
+++ js/inc.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('body').prepend('<header id="header"></header>')
+    $('body').append('<footer id="footer"></footer>')
+    $("#header").load("inc.html .head, .quick-box");
+    $("#footer").load("inc.html .footer-con");
+});
