e0f9055c230526d10313857a21b46558a6438c0f
yonghoon1013 <jyh10105@gmail.com>
2023-08-23 17:40:17 +0900

    dd

---
+++ js/slidelibrary.js
@@ -0,0 +1,21 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2
+    },
+    480: {
+      slidesPerView: 2
+    },
+    481: {
+        slidesPerView: 3
+      }
+}
+});
